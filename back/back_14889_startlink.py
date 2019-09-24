3385208633d55d07bcc185498b0ce7decaea9011
Yulim <yulim9592@gmail.com>
2019-09-25 00:39:02 +0900

    190924

--- back/back_14889_startlink.py
+++ back/back_14889_startlink.py
@@ -1,6 +1,56 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
-T = int(input())
-L = [list(map(int, input().split())) for _ in range(T)]
-print(L)
+
+def calc(T):
+    global mini
+    start = T[:N//2]
+    link = T[N//2:]
+    SA, LA = 0, 0
+
+    for i in range(len(start)-1):
+        for j in range(i+1, len(start)):
+            SA += L[start[i]][start[j]]
+            SA += L[start[j]][start[i]]
+            LA += L[link[i]][link[j]]
+            LA += L[link[j]][link[i]]
+    tmp = abs(SA-LA)
+
+    if mini > tmp:
+        mini = tmp
+
+
+def perm(k):
+    global start, link
+    for i in range(k, N):
+        if V[i] == 1:
+            continue
+        if (sorted(T[:N//2]) in start) or (sorted(T[N//2:]) in link):
+            start += [sorted(T[:N // 2])]
+            link += [sorted(T[N // 2:])]
+            T[k], T[i] = T[i], T[k]
+            V[i] = 1
+            perm(k + 1)
+            T[k], T[i] = T[i], T[k]
+            V[i] = 0
+        else:
+            start += [sorted(T[:N // 2])]
+            link += [sorted(T[N // 2:])]
+            T[k], T[i] = T[i], T[k]
+            V[i] = 1
+            perm(k + 1)
+            T[k], T[i] = T[i], T[k]
+            V[i] = 0
+            calc(T)
+
+
+N = int(input())
+L = [list(map(int, input().split())) for _ in range(N)]
+
+start, link = [], []
+V = [0] * N
+T = [i for i in range(N)]
+mini = 999999
+
+perm(0)
+print(mini)
