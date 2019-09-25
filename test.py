98b5e4abad729cd4c311cdc745a60e7a77c01530
Yulim <yulim9592@gmail.com>
2019-09-25 23:14:43 +0900

    190925

--- test.py
+++ test.py
@@ -1,21 +1,41 @@
-N = 4
-V = [0] * N
-T = [i for i in range(N)]
+import sys
+sys.stdin = open('input.txt', 'r')
 
-count = 0
-def perm(k):
-    global count
-    if k == N:
-        print(T)
-        count += 1
-        return
-    for i in range(k, N):
-        T[k], T[i] = T[i], T[k]
-        V[i] = 1
-        perm(k + 1)
-        T[k], T[i] = T[i], T[k]
-        V[i] = 0
+def BFS(y, x, L, V):
+    dir = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+    q = [[y, x]]
+    path = []
+
+    while q:
+        c = q.pop(0)
+        y, x = c[0], c[1]
+        if not V[y][x]:
+            path.append(L[y][x])
+            V[y][x] = 1
+
+        for d in dir:
+            idy, idx = c[0] + d[0], c[1] + d[1]
+            if 0 <= idy < N and 0 <= idx < N and not V[idy][idx]:
+                if abs(L[y][x] - L[idy][idx]) == 1:
+                    q.append([idy, idx])
+    return [len(path), min(path)]
 
 
-perm(0)
-print(count)
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    L = [list(map(int, input().split())) for _ in range(N)]
+    V = [[0]*N for _ in range(N)]
+    max_move = 0
+    min_cnt = N
+
+    for y in range(N):
+        for x in range(N):
+            if not V[y][x]:
+                move, cnt = BFS(y, x, L, V)
+                if move > max_move:
+                    max_move = move
+                    min_cnt = cnt
+                elif move == max_move and min_cnt > cnt:
+                    min_cnt = cnt
+    print('#%d %d %d' % (tc, min_cnt, max_move))
