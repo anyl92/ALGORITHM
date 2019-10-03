7dc8d7a0798b4cb0d4c74e8b6ace56d1a4671da2
Yulim <yulim9592@gmail.com>
2019-10-03 23:53:29 +0900

    191003

--- back/back_1541_parentheses.py
+++ back/back_1541_parentheses.py
@@ -1,36 +1,24 @@
 import sys
 sys.stdin = open('parentheses_input.txt', 'r')
 
-calc = list(input())
-print(calc)
-
 def calculater():
-    res = 0
     i = 0
-    tmp = ''
     while i < len(calc):
-        if calc[i] == '-' and calc[i] == '+':
-            if calc[i] == '+':
-                while calc[i] != '-':
-                    res = int(tmp)
-                    i += 1
-            else:
-
-        else:
-            tmp += calc[i]
+        if calc[i] == '-':
             i += 1
-
-            i += 1
-            while calc[i] != '-':
+            while i < len(calc):
                 if calc[i] == '+':
                     calc[i] = '-'
                 elif i + 1 < len(calc):
                     i += 1
                 else:
                     return calc
-        elif i + 1 < len(calc):
-            i += 1
-        else:
-            return calc
+        i += 1
+    return calc
 
-calculater()
+calc = list(input())
+print(calc)
+
+res = calculater()
+print(res)
+print(list(map(int, res.split('+'))))
