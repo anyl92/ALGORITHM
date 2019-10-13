7c28d156fa14a0168088c2a5d3624a16fb508bf1
Yulim <yulim9592@gmail.com>
2019-10-13 23:14:34 +0900

    191013

--- test.py
+++ test.py
@@ -1,17 +1,43 @@
-def combinations(n, list, combos=[]):
-    if combos is None:
-        combos = []
+import sys, time
+sys.stdin = open('input.txt', 'r')
+start = time.time()
 
-    if len(list) == n:
-        if combos.count(list) == 0:
-            combos.append(list)
-            combos.sort()
-        return combos
-    else:
-        for i in range(len(list)):
-            refined_list = list[:i] + list[i+1:]
-            combos = combinations(n, refined_list, combos)
-        return combos
+def func(r, c, d):
+    global cnt
 
-li = [1, 2, 3, 4]
-print(combinations(2, li))
+    dir = [[0, 1], [1, 0], [1, 1]]
+    if d == dir[0]:  # 가로일 때
+        dir[1] = False  # 아래로 90도 갈 수 없다
+    elif d == dir[1]:  # 세로일 때
+        dir[0] = False  # 옆으로 90도 갈 수 없다
+
+    for d in dir:
+        if d:
+            if 0 <= r+d[0] < N and 0 <= c+d[1] < N:
+                rr = r + d[0]
+                cc = c + d[1]
+                if d == [1, 1]:
+                    if L[r][cc] == 1 or L[rr][c] == 1:
+                        break
+
+                if L[rr][cc] == 0:
+                    L[rr][cc] = 2
+                    func(rr, cc, d)
+                    L[rr][cc] = 0
+                elif L[rr][cc] == 1:
+                    continue
+                elif L[rr][cc] == 3:
+                    cnt += 1
+                    return
+
+
+N = int(input())
+L = [list(map(int, input().split())) for _ in range(N)]
+
+L[0][1] = 2
+L[N-1][N-1] = 3
+
+cnt = 0
+func(0, 1, [0, 1])
+print(cnt)
+print('%f' % (time.time() - start))
