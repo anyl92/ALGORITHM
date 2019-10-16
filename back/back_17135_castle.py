fefafaf739acffde5c38aa48765207fbf838d3b7
Yulim <yulim9592@gmail.com>
2019-10-16 19:41:28 +0900

    191016

--- back/back_17135_castle.py
+++ back/back_17135_castle.py
@@ -2,67 +2,69 @@ import sys
 sys.stdin = open('17135.txt', 'r')
 
 
-def attack(r, T):  # 4, 0
-    global cnt
-    dir = [(0, -1), (-1, 0), (0, 1)]  # 좌 상 우
+def attack(T):
+    dir = [(0, -1), (-1, 0), (0, 1)]
+    V = [[0] * M for _ in range(N)]
     attack_list = []
+    cnt = 0
 
-    for c in T:  # 궁수의 개수(3) 만큼 반복
-        dcnt = 0
-        q = []
-        flag = True
-        if L[r][c]:
-            attack_list += [(r, c)]  # 현재위치에 적이 있으면 어택 배열에 추가
-        else:
-            q += [[r, c]]  # 현재위치 에서 q
-            while flag:
-                cur = q.pop()  # q를 꺼내서 좌표 지정
-                r = cur[0]
-                c = cur[1]
-                for d in dir:  # 다음 방향들(3)을 탐색
-                    if dcnt == AD-1:
-                        flag = False
+    for r in range(N - 1, -1, -1):
+        for c in T:
+            if L[r][c]:
+                attack_list += [(r, c)]
+            else:
+                dcnt = 1
+                q = [[r, c]]
+                flag = True
+
+                while q and flag:
+                    if dcnt == AD:
                         break
-                    rr = r + d[0]
-                    cc = c + d[1]
-                    if 0 <= rr < N and 0 <= cc < M and L[rr][cc]:
-                        attack_list += [(rr, cc)]
-                        flag = False
-                        break
-                    elif 0 <= rr < N and 0 <= cc < M and L[rr][cc]==0:
-                        q.append([rr, cc])  # q에 추가하여 다음 턴에 돈다
-                dcnt += 1  # bfs 한 번 돌 때 마다 카운트
 
-    for attack in attack_list:  # 한 번 턴에 죽일수 있는 적 리스트를 0으로 만들어서
-        L[attack[0]][attack[1]] = 0  # 다음 턴에 안 보게 만들어준다
+                    for _ in range(len(q)):
+                        cur = q.pop(0)
+                        V[cur[0]][cur[1]] = 1
 
-    return attack_list
+                        for d in dir:
+                            rr = cur[0] + d[0]
+                            cc = cur[1] + d[1]
+                            if 0 <= rr < N and 0 <= cc < M and V[rr][cc] == 0:
+                                if L[rr][cc]:
+                                    attack_list += [(rr, cc)]
+                                    flag = False
+                                    break
+                                else:
+                                    q.append([rr, cc])
+                        if not flag:
+                            break
+                    dcnt += 1
+
+        for x, y in set(attack_list):
+            if L[x][y]:
+                cnt += 1
+                L[x][y] = 0
+    return cnt
 
 
 def located(k, s):
     global res
-    total_attack_list = []
     if k == 3:
-        cnt = 0
-
-        for turn in range(N-1, -1, -1):
-            total_attack_list += attack(turn, T)
-            cnt = len(set(total_attack_list))
+        cnt = attack(T)
 
         if res < cnt:
             res = cnt
 
-        for i in total_attack_list:
-            L[i[0]][i[1]] = 1
-
-        return res
+        for i in range(N):
+            for j in range(M):
+                L[i][j] = CL[i][j]
+        return
 
     for i in range(s, M+(k-3)+1):
         T[k] = P[i]
         located(k+1, i+1)
 
 
-N, M, AD = map(int, input().split())  # 행, 열, 공격 거리
+N, M, AD = map(int, input().split())
 L = [list(map(int, input().split())) for _ in range(N)]
 CL = [[0]*M for _ in range(N)]
 
@@ -72,8 +74,7 @@ for i in range(N):
 
 res = 0
 
-T = [0, 0, 0]  # 조합이 저장될 배열
-P = [i for i in range(M)]  # 조합을 구할 수를 구한 배열 (0, 1, 2, 3, 4)
-
+T = [0, 0, 0]
+P = [i for i in range(M)]
 located(0, 0)
 print(res)
