e517a0149c139f22a490a85b826e42611b454eba
yulim <yulim9592@gmail.com>
2019-10-21 13:07:16 +0900

    file merge

--- swea/4835_sectionSum.py
+++ swea/4835_sectionSum.py
@@ -1,20 +1,23 @@
 import sys
-sys.stdin = open('sectionSum_input.txt', 'r')
+sys.stdin = open('sample_input(2).txt', 'r')
 
 def sectionSum(N, H):
-    s_H = 0
-    t_L = []
-    for i in range(0, N[1]):
-        s_H += H[i]
-    t_L.append(s_H)
-    for i in range(N[1], len(H)):
-        s_H = s_H - H[i-N[1]] + H[i]
-        t_L.append(s_H)
-    return max(t_L)-min(t_L)
+    sum_str = 0
+    sum_list = []
+    for i in range(N[1]):
+        sum_str += H[i]
+    sum_list.append(sum_str)
+
+    for j in range(N[1], len(H)):
+        sum_str = sum_str + H[j] - H[j-N[1]]
+        sum_list.append(sum_str)
+
+    return max(sum_list) - min(sum_list)
 
 T = int(input())
 for tc in range(1, T+1):
     N = list(map(int, input().split()))
     H = list(map(int, input().split()))
+
     res = sectionSum(N, H)
     print("#%d %d" % (tc, res))
