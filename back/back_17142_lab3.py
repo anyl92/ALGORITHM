2398ad6e09296c831b3eebd76d6a908c101dd162
yulim <yulim9592@gmail.com>
2019-10-23 15:13:43 +0900

    191023

--- back/back_17142_lab3.py
+++ back/back_17142_lab3.py
@@ -1,35 +1,37 @@
-import sys, collections, itertools
+import sys
 sys.stdin = open('17142.txt', 'r')
+
+import collections, itertools
 
 N, M = map(int, input().split())  # 크기 N, 바이러스 M
 L = [list(map(int, input().split())) for _ in range(N)]
-for k in L:
-    print(k)
 
 
 def bfs(dq, vst):
-    global min_cnt
+    global min_cnt, lose_virus
     cnt = 1
 
     while dq:
         for _ in range(len(dq)):
             x, y = dq.popleft()
-            print(x, y)
             for d in dir:
-                xx = x + d[0]
-                yy = y + d[1]
+                xx, yy = x + d[0], y + d[1]
                 if 0 <= xx < N and 0 <= yy < N:
-                    if L[yy][xx] == 0 and vst[yy][xx] == 0:
-                        # L[yy][xx] = cnt
+                    if vst[yy][xx] == 0:
                         dq.append((xx, yy))
+                        if L[yy][xx] == 2:
+                            vst[yy][xx] = 1
+                            continue
                         vst[yy][xx] = cnt
         cnt += 1
 
     for i in range(N):
         for j in range(N):
             if vst[i][j] == 0:
-                min_cnt = -1
+                lose_virus += 1
                 return
+
+    cnt = max(map(max, vst))
 
     if min_cnt > cnt:
         min_cnt = cnt
@@ -38,38 +40,38 @@ def bfs(dq, vst):
 
 virus = collections.deque()  # 바이러스 append
 vst = [[0] * N for _ in range(N)]  # visited, 함수 종료 처리
-
 dir = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # x, y 좌우상하
 min_cnt = 9999999999
+lose_virus = 0
+zero_cnt = 0
 
 for i in range(N):
     for j in range(N):
         if L[i][j] == 2:  # 바이러스 좌표추가, vst
             virus.append((j, i))
-            vst[i][j] = 1
         if L[i][j] == 1:  # 벽 vst
             vst[i][j] = 1
+        if L[i][j] == 0:
+            zero_cnt += 1
 
 P = [i for i in range(len(virus))]
 comb = list(itertools.combinations(P, M))
-# print(comb)
 
-# for k in vst:
-#     print(k)
-
-for j in range(len(comb)):
+for j in range(len(comb)):  # 조합 총개수 => 경우의수 만큼 for
     dq = collections.deque()
-
     cvst = [[0] * N for _ in range(N)]  # vst copy
     for y in range(N):
         for x in range(N):
             cvst[y][x] = vst[y][x]
-    # print(cvst)
 
-    for i in range(M):
+    for i in range(M):  # 바이러스 총개수 => 뽑힌 조합으로 dq에 추가
         dq.append(virus[comb[j][i]])
-        # cvst[virus[comb[j][i]][0]][virus[comb[j][i]][1]] = 1
+        cvst[virus[comb[j][i]][1]][virus[comb[j][i]][0]] = 1
     bfs(dq, cvst)
 
-
-print(min_cnt)
+if lose_virus == len(comb):
+    print(-1)
+elif not zero_cnt:
+    print(0)
+else:
+    print(min_cnt)
