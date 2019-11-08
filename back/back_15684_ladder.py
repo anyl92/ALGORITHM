8fa2769f8e35b43c3dcd04b8198e6991576521be
yulim <yulim9592@gmail.com>
2019-11-08 20:08:17 +0900

    191108

--- back/back_15684_ladder.py
+++ back/back_15684_ladder.py
@@ -4,6 +4,7 @@ sys.stdin = open('15684.txt', 'r')
 import itertools, collections
 W, M, H = map(int, input().split())
 L = [[0 for _ in range(W)] for _ in range(H)]
+tmp = -1
 
 for _ in range(M):
     a, b = map(int, input().split())
@@ -13,11 +14,53 @@ for l in L:
     print(l)
 
 
-def check():
-    return
+def check():  # r, c
+    for c in range(W):
+        cc = c  # c copy
+        r = 0
+        while cc < W and r < H:
+            while r + 1 < H and not L[r][cc]:
+                r += 1
+            if L[r][cc] == 2:
+                if L[r][cc+1] == 3:
+                    r += 1
+                    cc += 1
+                    continue
+            if L[r][cc] == 3:
+                if L[r][cc-1] == 2:
+                    r += 1
+                    cc += -1
+                    continue
+            r += 1
+        if c != cc:
+            return False
+    return True
+
+
+def construction(comb):
+    cnt = 0
+    for c in comb:
+        w, h = c[0], c[1]
+        if L[h][w]:
+            break
+        else:
+            L[h][w] = 2
+            L[h][w + 1] = 3
+            cnt += 1
+    else:
+        if check():
+            print(len(comb),'comb')
+            return len(comb)
+
+    for c in comb[:cnt]:
+        w, h = c[0], c[1]
+        L[h][w] = 0
+        L[h][w + 1] = 0
 
 
 def make_ladder():
+    global tmp
+
     ladder_list = []
     for h in range(H):
         for w in range(W-1):
@@ -25,19 +68,25 @@ def make_ladder():
                 if not L[h][w+1]:
                     ladder_list += [(w, h)]
 
-    for k in range(1, len(ladder_list)+1):
+    for comb in ladder_list:
+        tmp = construction([comb])
+        if tmp:
+            return
+
+    for k in range(2, 4):
         ladder_comb = collections.deque(itertools.combinations(ladder_list, k))
-
         while ladder_comb:
-            comb = ladder_comb.popleft()  # 조합 한 쌍
-            while comb:
-                w, h = comb.popleft()
-                L[h][w]
+            tmp = construction(ladder_comb.popleft())  # 조합 한 쌍
+            if tmp:
+                return
 
 
+if check():  # 맨 처음에 체크
+    print(0)
 
-
-check()
-# ok되면 리턴/끝
 make_ladder()
-# end
+print(tmp,'tmp')
+if tmp >= 0:
+    print(tmp)
+else:
+    print(-1)
