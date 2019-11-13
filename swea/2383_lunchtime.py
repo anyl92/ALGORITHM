42b01ffc353d0c18f1c1e0bdeb1b9747afb122a2
yulim <yulim9592@gmail.com>
2019-11-13 14:03:53 +0900

    191113

--- swea/2383_lunchtime.py
+++ swea/2383_lunchtime.py
@@ -3,92 +3,38 @@ sys.stdin = open('2383.txt', 'r')
 
 
 def step_down(dist, step):
+    # print('step', step)
+    # print(dist)
+    down = []
     cnt = dist[0]
-    full = []
 
-    if len(full) < 2:
-        forcnt = 1
-        for d in range(len(dist)-1):
-            if dist[d] != dist[d+1]:
-                break
-            else:
-                forcnt += 1
-
-    if len(full) < 3 and cnt == dist[0]:
-        for _ in range(forcnt):
-            full.append(step)
+    while dist:
+        while dist and dist[0] == cnt and len(down) < 3:
+            down.append(step + 1)
             dist.pop(0)
 
-    elif len(full) < 3 and cnt != dist[0]:
-        for _ in range(forcnt):
-            full.append(step-1)
-            dist.pop(0)
+        for k in range(len(down)):
+            down[k] -= 1
 
-    cnt += 1
-    for f in range(len(full)):
-        full[f] -= 1
+            if down[k] <= 0 and dist and dist[0] <= cnt:
+                down[k] = step
+                dist.pop(0)
 
+        for i in down:
+            if i <= 0:
+                down.pop(down.index(i))
 
-
-
-    # cnt = 0
-    # full = []
-    # fullmax = []
-    # mindist = dist[0]
-    # stepval = L[step[0]][step[1]]
-    #
-    # for k in range(len(dist)-1):
-    #     comp = dist[k + 1] - dist[k]
-    #     if comp == 0 or comp == 1:
-    #         continue
-    #     else:
-    #         cnt += comp
-    #
-    # while True:
-    #     if len(full) < 3 and dist:
-    #         popval = dist.pop(0)
-    #         full.append(popval)
-    #         fullmax.append(popval + stepval + 1)
-    #
-    #     while len(full) < 3 and dist and popval == dist[0]:
-    #         dist.pop(0)
-    #         full.append(popval)
-    #         fullmax.append(popval + stepval + 1)
-    #
-    #     for f in range(len(full)):
-    #         full[f] += 1
-    #
-    #     while full and fullmax[0] == full[0]:
-    #         full.pop(0)
-    #         fullmax.pop(0)
-    #         if not full and not dist:
-    #             return mindist + cnt + 1
-    #     cnt += 1
-
-
-def step_find(q, step):
-    vst = [[0 for _ in range(N)] for _ in range(N)]
-    cnt = 1
-    while q:
-        for _ in range(len(q)):
-            r, c = q.pop(0)
-            for d in dir:
-                rr = r + d[0]
-                cc = c + d[1]
-                if 0 <= rr < N and 0 <= cc < N and not vst[rr][cc]:
-                    if rr == step[0] and cc == step[1]:
-                        return cnt
-                    vst[rr][cc] = 1
-                    q.append([rr, cc])
         cnt += 1
-
+    if down:
+        return cnt + max(down)
+    else: return cnt
 
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     L = [list(map(int, input().split())) for _ in range(N)]
     dir = [(1, 0), (-1, 0), (0, 1), (0, -1)]  # 하상우좌
-    res = 9999
+    res = 9999999999
 
     per, step = [], []
     for i in range(N):
@@ -118,30 +64,27 @@ for tc in range(1, T+1):
                     step2.pop(step2.index(comb2))
                     break
 
-        print(step1, step2)
-
-        # 구한 step1과 step2에서 bfs 동작 - 거리 계산
+        # 거리 구함
         for s1 in step1:
-            step1_dist += [step_find([s1], step[0])]
+            step1_dist += [abs(s1[0]-step[0][0]) + abs(s1[1]-step[0][1])]
         for s2 in step2:
-            step2_dist += [step_find([s2], step[1])]
-
+            step2_dist += [abs(s2[0] - step[1][0]) + abs(s2[1] - step[1][1])]
+        # print()
         # 계단 내려감
         step1_cnt, step2_cnt, step_cnt = 0, 0, 0
         if step1_dist:
-            step1_cnt = step_down(sorted(step1_dist), step[0])
+            step1_cnt = step_down(sorted(step1_dist), L[step[0][0]][step[0][1]])
         if step2_dist:
-            step2_cnt = step_down(sorted(step2_dist), step[1])
-        if step1_cnt >= step2_cnt:
-            step_cnt = step1_cnt
-        else:
-            step_cnt = step2_cnt
-        print(step1_cnt, step2_cnt)
+            step2_cnt = step_down(sorted(step2_dist), L[step[1][0]][step[1][1]])
+
+        step_cnt = max(step1_cnt, step2_cnt)
+        # print(step_cnt)
+        # if step1_cnt >= step2_cnt:
+        #     step_cnt = step1_cnt
+        # else:
+        #     step_cnt = step2_cnt
+
         if res > step_cnt:
             res = step_cnt
 
-        # 초기화
-        step2 = []
-        for c in per:
-            step2 += [c]
-    print(res, 'res')
+    print("#%d %d" % (tc, res))
--- test.py
+++ test.py
@@ -1,31 +1,38 @@
-def step_down(dist):
-    step = 7
+# def step_down(dist, step):
+#     down = []
+#     cnt = dist[0]
+#
+#     while dist:
+#         while dist and dist[0] == cnt and len(down) < 3:
+#             down.append(step + 1)
+#             dist.pop(0)
+#
+#         for k in range(len(down)):
+#             down[k] -= 1
+#
+#             if down[k] <= 0 and dist and dist[0] < cnt:
+#                 down[k] = step
+#                 dist.pop(0)
+#
+#         for i in down:
+#             if i == 0:
+#                 down.pop(i)
+#
+#         cnt += 1
+#     print(cnt + max(down))
+#     return cnt + max(down)
 
-    cnt = dist[0]
-    full = []
+# dist = [3, 6, 7, 8]
+# dist = [2, 2, 2, 3]
+# dist = [2, 4, 6, 7]
 
-    while dist:
-        if len(full) < 2:
-            forcnt = 1
-            for d in range(len(dist) - 1):
-                if dist[d] != dist[d + 1]:
-                    break
-                else:
-                    forcnt += 1
+# dist = [4, 7]
+# dist = [3, 3, 6, 7, 8]
 
-        if len(full) < 3 and cnt == dist[0]:
-            for _ in range(forcnt):
-                full.append(step)
-                dist.pop(0)
+# dist = [6, 3]
+# dist = [4, 5, 4, 12, 7]
 
-        elif len(full) < 3 and cnt != dist[0]:
-            for _ in range(forcnt):
-                full.append(step - 1)
-                dist.pop(0)
+# step_down([6, 3], 7)
 
-        cnt += 1
-        for f in range(len(full)):
-            full[f] -= 1
-
-dist = [3, 6, 7, 8]
-step_down(dist)
+a = []
+print(a[0])
