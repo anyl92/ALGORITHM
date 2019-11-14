94b124bd74956afe98f95f8d91b88afbe932d37b
yulim <yulim9592@gmail.com>
2019-11-14 17:44:45 +0900

    191114

--- swea/1868_poping.py
+++ swea/1868_poping.py
@@ -1,6 +1,7 @@
 import sys
 sys.stdin = open('1868.txt', 'r')
 
+
 def display(arr):
     for i in range(len(arr)):
         for j in range(len(arr[i])):
@@ -8,17 +9,18 @@ def display(arr):
         print()
     print()
 
+
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     L = [list(map(str, input())) for _ in range(N)]
+    vst = [[0 for _ in range(N)] for _ in range(N)]
     dir = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]  # 내천
 
 
     def bfs(j, i):
         q = [(j, i)]
         L[i][j] = 0
-        vst = [[0 for _ in range(N)] for _ in range(N)]
         vst[i][j] = 1
 
         while q:
@@ -27,7 +29,7 @@ for tc in range(1, T+1):
             mine = 0
             for d in dir:
                 jj, ii = j+d[0], i+d[1]
-                if 0 <= jj < N and 0 <= ii < N and not vst[ii][jj]:
+                if 0 <= jj < N and 0 <= ii < N and not vst[ii][jj]:  #
                     if L[ii][jj] != '*':
                         zero += [(jj, ii)]
                     elif L[ii][jj] == '*':
@@ -43,47 +45,22 @@ for tc in range(1, T+1):
                 vst[i][j] = 1
 
 
-    def find_zero(j, i, k):
-        global flag
-        if k == i:
-            flag = True
-            return
-
-        if L[i][j] != '*' and L[i][j + 1] != '*' and L[i][j + 2] != '*':
-            find_zero(j, i+1, k)
-        else:
-            flag = False
-            return
-
-
     cnt = 0
-    for i in range(N-2):
-        for j in range(N-2):
-            flag = False
-            if L[i][j] != '*' and L[i][j + 1] != '*' and L[i][j + 2] != '*':
-                find_zero(j, i + 1, i + 3)
-                if flag and L[i+1][j+1] == '.':
-                    bfs(j+1, i+1)
+    for i in range(N):
+        for j in range(N):
+            if L[i][j] == '.':
+                for d in dir:
+                    jj, ii = j + d[0], i + d[1]
+                    if 0 <= jj < N and 0 <= ii < N:
+                        if L[ii][jj] == '*':
+                            break
+                else:
+                    bfs(j, i)
                     cnt += 1
-    print(cnt, 'firstcnt')
-    display(L)
-
-    summ =0
-    for l in L:
-        ''.join(map(str, l)).count('.')
-        summ += ''.join(map(str, l)).count('.')
-    print(summ, 'summ')
-        # print(' '.join(map(str, l)))
-    print()
-
-
 
     for i in range(N):
         for j in range(N):
             if L[i][j] == '.':
-                bfs(j, i)
                 cnt += 1
-    display(L)
 
-
-    print(cnt)
+    print('#%d %d' % (tc, cnt))
