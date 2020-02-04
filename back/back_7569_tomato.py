c6d3a8c7174f4e6b87fe37468ef54ab281167dad
yulim <yulim9592@gmail.com>
2020-02-04 23:51:42 +0900

    200204

---
+++ back/back_7569_tomato.py
@@ -0,0 +1,57 @@
+import sys
+sys.stdin = open('7569.txt', 'r')
+
+from collections import deque
+
+M, N, H = map(int, input().split())  # 가로 세로 높이
+L = [[list(map(int, input().split())) for _ in range(N)] for _ in range(H)]
+
+
+def check(day):
+    for z in range(H):
+        for y in range(N):
+            for x in range(M):
+                if L[z][y][x] == 0:
+                    return 1  # 더 돌아
+    return 0  # while 종료
+
+
+day = 1
+flag = 0
+dir = [[0, 0, -1], [0, 0, 1], [0, -1, 0], [0, 1, 0], [-1, 0, 0], [1, 0, 0]]
+def bfs(q):
+    global day, flag
+    v = [[[0]*M for _ in range(N)] for _ in range(H)]
+    while q:
+        for _ in range(len(q)):
+            x, y, z = q.popleft()
+            for d in dir:
+                xx, yy, zz = x+d[0], y+d[1], z+d[2]
+                if 0 <= xx < M and 0 <= yy < N and 0 <= zz < H:
+                    if v[zz][yy][xx] == 0 and L[zz][yy][xx] == 0:
+                        v[zz][yy][xx] = 1
+                        L[zz][yy][xx] = 1
+                        q.append((xx, yy, zz))
+        chk = check(day)
+        if chk == 0:
+            flag = 1  # 0없으니 나가서 day나 세라
+            return day
+        else:
+            day += 1  # 하루가 지나염
+    return -1  # 0이 있어 ...
+
+
+zero_cnt = 0
+bfs_list = deque()
+for z in range(H):
+    for y in range(N):
+        for x in range(M):
+            if L[z][y][x] == 1:
+                bfs_list.append((x, y, z))
+            if L[z][y][x] == 0:
+                zero_cnt += 1
+
+if zero_cnt:
+    print(bfs(bfs_list))
+else:
+    print(0)
--- back/back_7576_tomato.py
+++ back/back_7576_tomato.py
@@ -1,60 +1,68 @@
 import sys
 sys.stdin = open('7576.txt', 'r')
 
-T = int(input())
-for tc in range(1, T+1):
-    C, R = map(int, input().split())
-    L = [list(map(int, input().split())) for _ in range(R)]
-    print(C, R, L)
+from collections import deque
 
-    def check(day):
-        for r in range(R):
-            for c in range(C):
-                if L[r][c] == 0:
-                    return day + 1
-        return -1
-
-    day = 0
-    flag = 0
-    def bfs(c, r, day):
-        q = [(c, r)]
-        v = [[0]*C for _ in range(R)]
-        v[r][c] = 1
-        while q:
-            for _ in range(len(q)):
-                x, y = q.pop(0)
-                for dx, dy in [[-1, 0], [1, 0], [0, -1], [0, 1]]:
-                    xx, yy = x+dx, y+dy
-                    if 0 <= xx < C and 0 <= yy < R:
-                        if v[yy][xx] == 0 and L[yy][xx] == 0:
-                            v[yy][xx] = 1
-                            L[yy][xx] = 1
-                            q.append((xx, yy))
-            # print('day')
-            # print(q)
-            # for z in L:
-            #     print(z)
-            chk = check(day)
-            if chk == -1:
-                flag = 1
-                return day
-            else:
-                day = chk
-        return day
+# T = int(input())
+# for tc in range(1, T+1):
+C, R = map(int, input().split())
+L = [list(map(int, input().split())) for _ in range(R)]
 
 
+def check(day):
     for r in range(R):
         for c in range(C):
-            if L[r][c] == 1:
-                # print('in', r, c)
-                tmp = bfs(c, r, 1)
-                if tmp > day:
-                    day = tmp
+            if L[r][c] == 0:
+                return 1
+    return 0
 
-    for z in L:
-        print(z)
-    print(flag)
+
+day = 1
+flag = 0
+def bfs(q):
+    global day, flag
+    v = [[0]*C for _ in range(R)]
+    while q:
+        for _ in range(len(q)):
+            x, y = q.popleft()
+            v[y][x] = 1 
+            for dx, dy in [[-1, 0], [1, 0], [0, -1], [0, 1]]:
+                xx, yy = x+dx, y+dy
+                if 0 <= xx < C and 0 <= yy < R:
+                    if v[yy][xx] == 0 and L[yy][xx] == 0:
+                        v[yy][xx] = 1
+                        L[yy][xx] = 1
+                        q.append((xx, yy))
+        # for x in v:
+        #     print(x)
+        # print('day')
+        # print(q)
+        # for z in L:
+        #     print(z)
+        # chk = check(day)
+        if check(day) == 0:
+            flag = 1
+            return day
+        else:
+            day += 1
+    return day
+
+
+zerocnt = 0
+bfs_list = deque()
+for r in range(R):
+    for c in range(C):
+        if L[r][c] == 1:
+            bfs_list.append((c, r))
+        if L[r][c] == 0:
+            zerocnt += 1
+# print(bfs_list)            
+
+if zerocnt:
+    tmp = bfs(bfs_list)
     if flag:
-        print(day)
+        print(tmp)
     else:
         print(-1)
+else:
+    print(0)
