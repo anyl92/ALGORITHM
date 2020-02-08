889921a0c3dc3f168d332d8cbe4054358300920a
yulim <yulim9592@gmail.com>
2020-02-08 19:44:12 +0900

    TT

--- back/back_17143_fishing.py
+++ back/back_17143_fishing.py
@@ -20,16 +20,54 @@ def eat(L):
 
 
 def reset():
-    L = deque(deque(deque() for _ in range(C)) for _ in range(R))
+    L = [[[] for _ in range(C)] for _ in range(R)]
     for i in info:
         L[i[0]-1][i[1]-1].append((i[2], i[3], i[4]))
     return L
 
 
 def move(L):
-    for shark in info:  # [4, 1, 3, 3, 8]
-        y, x = (shark[0], shark[1])  # 현재 rc
-        for _ in range(shark[2]):
+    for shark in info:
+        y, x, s, d, z = map(int, shark)  # 현재 rcs
+        s = s % (2 * (R - 1))
+        # if d == 1:  # 위
+        #     if (y - 1) < s:
+        #         if (s // R) % 2:
+        #             shark[3] = 2
+        #             shark[0] = y + (s % R)
+        #         else:
+        #             shark[0] = y - (s % R)
+        #     else:
+        #         shark[0] = y - s
+        # elif d == 2:  # 아래
+        #     if (R - y) < s:
+        #         if (s // R) % 2:
+        #             shark[3] = 1
+        #             shark[0] = y - (s % R)
+        #         else:
+        #             shark[0] = y + (s % R)
+        #     else:
+        #         shark[0] = y + s
+        # elif d == 3:  # 오른
+        #     if (C - x) < s:
+        #         if (s // C) % 2:
+        #             shark[3] = 4
+        #             shark[1] = x - (s % C)
+        #         else:
+        #             shark[1] = x + (s % C)
+        #     else:
+        #         shark[1] = x + s
+        # elif d == 4:  # 왼
+        #     if (x - 1) < s:
+        #         if (s // C) % 2:
+        #             shark[3] = 3
+        #             shark[1] = x + (s % C)
+        #         else:
+        #             shark[1] = x - (s % C)
+        #     else:
+        #         shark[1] = x - s
+
+        for _ in range(s):
             d = dir[shark[3]-1]
             dy, dx = y+d[0], x+d[1]
             if not (0 < dy <= R and 0 < dx <= C):
