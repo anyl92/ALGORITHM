0f7f96ae10659f5ccd53dee4a41caee8c7c5812f
anyl92 <yulim9592@gmail.com>
2020-04-13 20:59:17 +0900

    200413

--- back/back_2805_tree.py
+++ back/back_2805_tree.py
@@ -3,5 +3,26 @@ sys.stdin = open('2805.txt', 'r')
 
 N, M = list(map(int, input().split()))
 L = list(map(int, input().split()))
-print(N, M, L)
 
+
+def dicision(mid):
+    res = 0
+    for i in range(N):
+        res += max(L[i] - mid, 0)
+    if M <= res:
+        return True
+    return False
+
+
+def binary_search():
+    start = 0
+    end = max(L)
+    while start <= end:
+        mid = (start+end)//2
+        if (dicision(mid)):
+            start = mid + 1
+        else:
+            end = mid - 1
+    return end
+
+print(binary_search())
