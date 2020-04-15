d8de7877f8c98dc7ac1f56040dc612c68ee0ee85
anyl92 <yulim9592@gmail.com>
2020-04-15 23:51:33 +0900

    test

---
+++ back/back_2110_wifi.py
@@ -0,0 +1,6 @@
+import sys
+sys.stdin = open('2110.txt', 'r')
+
+home, wifi = map(int, input().split())
+L = [int(input()) for _ in range(home)]
+print(home, wifi, L)
--- back/back_2805_tree.py
+++ back/back_2805_tree.py
@@ -9,20 +9,19 @@ def dicision(mid):
     res = 0
     for i in range(N):
         res += max(L[i] - mid, 0)
-    if M <= res:
-        return True
-    return False
+    return bool(M <= res)
 
 
 def binary_search():
     start = 0
     end = max(L)
     while start <= end:
-        mid = (start+end)//2
-        if (dicision(mid)):
+        mid = (start+end) // 2
+        if dicision(mid):
             start = mid + 1
         else:
             end = mid - 1
     return end
+
 
 print(binary_search())
