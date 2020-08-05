9714b522421fbfe917247c11b40e99b642f47b5b
anyl92 <yulim9592@gmail.com>
2020-08-05 17:38:05 +0900

    200805

--- back/back_19237_Adult Shark.py
+++ back/back_19237_Adult Shark.py
@@ -4,52 +4,84 @@ sys.stdin = open('19237.txt', 'r')
 N, M, k = map(int, input().split())
 L = [list(map(int, input().split())) for _ in range(N)]
 d = list(map(int, input().split()))
-print(N, M, k, d)
 P = [[] for _ in range(M)]
 for m in range(M):
     for _ in range(4):
         P[m] += [list(map(int, input().split()))]
-print(P)
 
-# 초기 배열
+# 초기배열
 dir = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-smell = [[() for _ in range(N)] for _ in range(N)]
-move = [[() for _ in range(N)] for _ in range(N)]
-time = 1
+smell = [[[] for _ in range(N)] for _ in range(N)]
+move = [[[] for _ in range(N)] for _ in range(N)]
 for i in range(N):
     for j in range(N):
         if L[i][j]:
             cur = L[i][j]
-            smell[i][j] = cur, k
-            move[i][j] = cur, d[cur-1]
+            smell[i][j] = [cur, k]
+            L[i][j] = [cur, d[cur-1]]
+time = 0
 
-# 이동하기
-for i in range(N):
-    for j in range(N):
-        if move[i][j]:
-            print(move[i][j])
-            cur_n = move[i][j][0]
-            cur_d = move[i][j][1]
-            cur_p = P[cur_n - 1][cur_d - 1]
-            # 보고있는 방향에 따라 이동할 위치방향 바꿔주기
-            if cur_d == 1:  # 위 1234->4312
-                for a in range(4):
-                    if cur_p[a] == 1: cur_p[a] = 4
-                    elif cur_p[a] == 2: cur_p[a] = 3
-                    elif cur_p[a] == 3: cur_p[a] = 1
-                    else: cur_p[a] = 2
-            elif cur_d == 2:  # 아래 1234->3421
-                for a in range(4):
-                    if cur_p[a] == 1: cur_p[a] = 3
-                    elif cur_p[a] == 2: cur_p[a] = 4
-                    elif cur_p[a] == 3: cur_p[a] = 2
-                    else: cur_p[a] = 1
+while True:
+    if time > 1000:
+        print(-1)
+        break
 
-            for p in cur_p:
-                idx, jdx = dir[p-1]
-                if 0 <= i+idx < N and 0 <= j+jdx < N:
-                    move[i+idx][j+jdx] = move[i][j]
-                    # 옮길 수 있으면 옮기고 포문 나가기
-                # 안되면 다음 방향
+    # 이동하기
+    for i in range(N):
+        for j in range(N):
+            if L[i][j]:
+                cur_n = L[i][j][0]
+                cur_d = L[i][j][1]
+                cur_p = P[cur_n - 1][cur_d - 1]
 
-# 배열 하나로 다시 고치기
+                onego_list, twogo_list = [], []
+                for p in cur_p:
+                    idx, jdx = dir[p-1]
+                    if 0 <= i+idx < N and 0 <= j+jdx < N:
+                        if smell[i+idx][j+jdx] == []:
+                            onego_list += [[i + idx, j + jdx, p]]
+                            break
+                        elif smell[i+idx][j+jdx][0] == cur_n:
+                            twogo_list += [[i + idx, j + jdx, p]]
+
+                if onego_list != []:
+                    ii, jj, pp = onego_list.pop(0)
+                else:
+                    ii, jj, pp = twogo_list.pop(0)
+
+                if move[ii][jj]:
+                    cur = move[ii][jj]
+                    new = L[i][j]
+                    if cur[0] < new[0]:
+                        move[ii][jj] = [cur[0], pp]
+                    else:
+                        move[ii][jj] = [new[0], pp]
+                else:
+                    move[ii][jj] = [L[i][j][0], pp]
+                move[i][j] = []
+
+    # 냄새빼기
+    for a in range(N):
+        for b in range(N):
+            if smell[a][b]:
+                smell[a][b][1] -= 1
+                if smell[a][b][1] == 0:
+                    smell[a][b] = []
+
+    # 배열복사 냄새깔기
+    L = [[0 for _ in range(N)] for _ in range(N)]
+    for a in range(N):
+        for b in range(N):
+            if move[a][b]:
+                L[a][b] = move[a][b]
+                smell[a][b] = [move[a][b][0], k]
+
+    time += 1
+
+    x_list = []
+    for x in sum(L, []):
+        if x:
+            x_list += [x[0]]
+    if len(x_list) == 1 and x_list[0] == 1:
+        print(time)
+        break
