e7fb0d8a3f0d8b9d547dea8e6c846348297a2e22
anyl92 <yulim9592@gmail.com>
2020-09-04 17:06:15 +0900

    200904

---
+++ baek/baek_1747.py
@@ -0,0 +1,20 @@
+def primary(n):
+    for i in range(2, n):
+        if n % i != 0:
+            continue
+        return False
+    return True
+
+
+def pelindrome(n):
+    if n[::-1] != n:
+        return False
+    return True
+
+
+num = int(input())
+while True:
+    if pelindrome(str(num)) and primary(num):
+        print(num)
+        break
+    num += 1
