8f4f55049b890227b8cf7fa68714b6f24c456779
anyl92 <yulim9592@gmail.com>
2020-09-08 04:46:31 +0900

    200908

---
+++ baek/baek_11286_heap.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open('11286.txt', 'r')
+
+N = int(input())
+L = [int(input()) for _ in range(N)]
+
+res = []
+for x in L:
+    if x != 0:
+        res.append([x, abs(x)])
+    else:
+        if res:
+            if len(res) > 1:
+                res.sort(key=lambda y: (y[1], y[0]))
+            tmp, _ = res.pop(0)
+            print(tmp)
+        else:
+            print(0)
