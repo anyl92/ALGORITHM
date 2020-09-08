4bc97f7a171ce47f7224fd57f38c5087832a64e1
anyl92 <yulim9592@gmail.com>
2020-09-08 17:46:31 +0900

    200908

--- baek/baek_11286_heap.py
+++ baek/baek_11286_heap.py
@@ -1,19 +1,30 @@
 import sys
+import heapq
 sys.stdin = open('11286.txt', 'r')
-
-N = int(input())
-L = [int(input()) for _ in range(N)]
-
-
-res = []
-for x in L:
-    if x != 0:
-        res.append([x, abs(x)])
+heap = []
+N = int(sys.stdin.readline())
+for _ in range(N):
+    x = int(sys.stdin.readline())
+    if x:  # 절댓값과 원래값 넣기
+        if x > 0:
+            heapq.heappush(heap, (x, x))
+        else:
+            heapq.heappush(heap, (-x, x))
     else:
-        if res:
-            if len(res) > 1:
-                res.sort(key=lambda y: (y[1], y[0]))
-            tmp, _ = res.pop(0)
-            print(tmp)
+        if heap:
+            print(heapq.heappop(heap)[1])
         else:
             print(0)
+
+# res = []
+# for x in L:
+#     if x != 0:
+#         res.append([x, abs(x)])
+#     else:
+#         if res:
+#             if len(res) > 1:
+#                 res.sort(key=lambda y: (y[1], y[0]))
+#             tmp, _ = res.pop(0)
+#             print(tmp)
+#         else:
+#             print(0)
