0550600995de42f457c57ed86b21748b5fb44d92
anyl92 <yulim9592@gmail.com>
2020-09-22 16:56:41 +0900

    200922

--- baek/baek_2812_big.py
+++ baek/baek_2812_big.py
@@ -2,26 +2,50 @@ import sys
 sys.stdin = open('2812.txt', 'r')
 
 N, K = map(int, input().split())
-L = list(input())
-counting_list = [0 for _ in range(10)]
-for i in range(N-K):
-    counting_list[int(L[i])] += 1
+L = list(map(int, input()))
 
-res_list = []
-for count in range(len(counting_list)-1, -1, -1):
-    if counting_list[count]:
-        idx_list = list(filter(lambda x:L[x] == str(count), range(N-K-1)))
+s = []
+cnt = 0
 
-        for idx in idx_list:
-            tmp_list = L[idx:idx+N]
-            j = 1
-            while len(tmp_list) != N-K:
-                if tmp_list[i] < tmp_list[i+1]:
-                    tmp_list.pop(i)
-                else:
-                    
-                # tmp_list.pop(tmp_list.index(min(tmp_list)))
-                j += 1
-            res_list.append(int(''.join(tmp_list)))
-            print(res_list)
-print(max(res_list))
+for i in L:
+    while s and cnt < K and s[-1] < i:
+        s.pop()
+        K -= 1
+    s.append(i)
+
+while cnt < K:
+    s.pop()
+    K -= 1
+
+for res in s:
+    print(res, end='')
+
+# res = ''.join(L[K:])
+# if N-K-1 == 0:
+#     print(str(max(L)))
+#
+# for i in range(N-K-1):
+#     res_list = L[i:i+N]
+#     if int(res_list[0]) < int(res[0]):
+#         continue
+#     if len(res_list) < N-K:
+#         continue
+#
+#     j = 1
+#     while len(res_list) > N-K:
+#         if len(res_list) == 2:
+#             if res_list[0] < res_list[1]:
+#                 res_list.pop(0)
+#             else:
+#                 res_list.pop()
+#         elif res_list[j] < res_list[j+1]:
+#             res_list.pop(j)
+#         else:
+#             res_list.pop(res_list.index(min(res_list)))
+#             j -= 1
+#         j += 1
+#
+#     tmp = ''.join(res_list)
+#     if int(res) < int(tmp):
+#         res = tmp
+# print(res)
